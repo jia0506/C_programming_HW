5859a8e48a434d0e9ba40e83bda5b8771c2cf16f
jia0506 <jia050612@naver.com>
2021-11-10 17:43:08 +0900

    8-6

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,19 +1,54 @@
 ﻿#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
-double f_rand();
 
 int main(void) {
 
-	srand((unsigned)time(NULL));
-	for (int i = 0; i < 10; i++) {
-		printf("%lf  ", f_rand());
-	}
+	int n;
+	printf("정수를 입력하시오:");
+	scanf_s("%d", &n);
+
+	if (even(n) == 1)
+		printf("even()의 결과: 짝수\n");
+	else
+		printf("even()의 결과: 홀수\n");
+
+	printf("absolute()의 결과:%d\n", absolute(n));
+
+	if (sign(n) == -1)
+		printf("sign()의 결과: 음수\n");
+	else if (sign(n) == 0)
+		printf("sign()의 결과: 0\n");
+	else if (sign(n) == 1)
+		printf("sign()의 결과: 양수\n");
+
+	printf("even(n)*absolute(n)*sign(n)의 결과: %d\n", even(n) * absolute(n) * sign(n));
+
 
 }
 
-double f_rand() {
+int even(int n) {
 
-	return rand() / (double)RAND_MAX * 2.2 - 0.5;
+	if (n % 2 == 0)
+		return 1;
+
+	else
+		return 0;
+}
+
+int absolute(int n) {
+
+	if (n < 0)
+		return -n;
+	else
+		return n;
+}
+
+int sign(int n) {
+
+	if (n < 0)
+		return -1;
+	else if (n == 0)
+		return 0;
+	else
+		return 1;
+
 }
