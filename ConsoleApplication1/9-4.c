f1ccb3337d5d492095a8de942cc81284a161af32
jia0506 <jia050612@naver.com>
2021-11-11 02:07:31 +0900

    9-5

--- ConsoleApplication1/9-4.c
+++ ConsoleApplication1/9-4.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int get_random() {
-	static int inited = 0;
-	if (inited == 0) {
-		printf("초기화 실행\n");
-		inited = 1;
-		srand((unsigned)time(NULL));
-	}
-	return rand();
-}
-
-int main() {
-	printf("%d\n", get_random());
-	printf("%d\n", get_random());
-	printf("%d", get_random());
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//int get_random();
+//
+//int main() {
+//	printf("%d\n", get_random());
+//	printf("%d\n", get_random());
+//	printf("%d", get_random());
+//	return 0;
+//}
+//
+//int get_random() {
+//	static int inited = 0;
+//	if (inited == 0) {
+//		printf("초기화 실행\n");
+//		inited = 1;
+//		srand((unsigned)time(NULL));
+//	}
+//	return rand();
+//}
