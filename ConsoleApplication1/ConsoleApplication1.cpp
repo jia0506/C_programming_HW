37376a5250545b1d68e03f3f5984eae0e576265f
jia0506 <jia050612@naver.com>
2021-11-10 17:38:41 +0900

    8장 11번

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+double f_rand();
+
+int main(void) {
+
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++) {
+		printf("%lf  ", f_rand());
+	}
+
+}
+
+double f_rand() {
+
+	return rand() / (double)RAND_MAX * 2.2 - 0.5;
+}
