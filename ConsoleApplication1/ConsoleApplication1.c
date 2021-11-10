95711b59f24b1176ca8f76c7454a3f3ab96cc1fa
jia0506 <jia050612@naver.com>
2021-11-10 18:16:34 +0900

    9-4

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,16 @@
+﻿//#include<stdio.h>
+//#include<math.h>
+//
+//
+//double cos_degree(double degree) {
+//	double PI = 3.141592;
+//
+//	return cos(PI*degree/180.0);
+//
+//}
+//
+//int main(void) {
+//	for (int i = 0; i <= 180;i += 10) {
+//		printf("cos(%3d)의 값은 %lf\n", i, cos_degree(i));
+//	};
+//}
